c667e4843bdcb6ad5752c41e1b86513bc2e18a08
lukdyn <107786261+lukdyn@users.noreply.github.com>
2024-04-26 15:38:31 +0200

    poprawiony kod panelu logowania

---
+++ backend/log-module.js
@@ -0,0 +1,16 @@
+var nick = " ";
+var haslo = " ";
+
+
+nick=prompt("Nick: " +nick);
+haslo=prompt("Hasło: " +haslo);
+
+if (nick == "nick" && haslo == "123")
+{
+alert("Witamy");
+
+ }
+ else
+ alert("Błędny login lub hasło");
+/*pętla /*
+}
