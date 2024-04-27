1a7dbc6bcae7cacdc3a679a4e419ff5ddff0bc6e
adrianbanaszczyk2 <161241194+adrianbanaszczyk2@users.noreply.github.com>
2024-04-28 01:38:48 +0200

    Update booksRoute.js

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -30,20 +30,6 @@ router.post('/', async (request, response) => {
   }
 });
 
-// Route for Get All Books from database
-router.get('/', async (request, response) => {
-  try {
-    const books = await Book.find({});
-
-    return response.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
 
 // Route for Get One Book from database by id
 router.get('/:id', async (request, response) => {
