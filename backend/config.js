a2af1ffdc2ec7fc66c14fd391c2470cce3170633
Eny02 <105815202+Eny02@users.noreply.github.com>
2024-04-27 21:28:11 +0200

    zmiana mongodburl

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://root:root@meritosfullstack.dkmedl3.mongodb.net/books-collection?retryWrites=true&w=majority&appName=meritosfullstack";
+  "mongodb+srv://silvercrestpl:<password>@projekt1cluster.wqydyda.mongodb.net/?retryWrites=true&w=majority&appName=Projekt1Cluster";
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial.
