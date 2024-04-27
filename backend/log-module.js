0098c6141703421c03daf3db981c3b4760e9ba6a
lukdyn <107786261+lukdyn@users.noreply.github.com>
2024-04-27 22:25:17 +0200

    poprawa pliku log-module.js

--- backend/log-module.js
+++ backend/log-module.js
@@ -1,16 +1,11 @@
 var nick = " ";
 var haslo = " ";
 
+nick = prompt("Nick: " + nick);
+haslo = prompt("Hasło: " + haslo);
 
-nick=prompt("Nick: " +nick);
-haslo=prompt("Hasło: " +haslo);
-
-if (nick == "nick" && haslo == "123")
-{
-alert("Witamy");
-
- }
- else
- alert("Błędny login lub hasło");
-/*pętla /*
+if (nick == "nick" && haslo == "123") {
+  alert("Witamy");
+} else {
+  alert("Błędny login lub hasło");
 }
